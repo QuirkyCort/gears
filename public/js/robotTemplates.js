c36fa8cef689e9730cb8f1b178e79153656cf624
Cort <cort@cortscorner.net>
2020-07-09 17:52:42 +0800

    * Added maze runner robot * Increased input and output ports to 20

--- public/js/robotTemplates.js
+++ public/js/robotTemplates.js
@@ -250,4 +250,109 @@ var robotTemplates = [
       }
     ]
   },
+  {
+    name: 'maze',
+    shortDescription: 'Maze Runner',
+    longDescription:
+      '<p>This robot is equipped with three ultrasonic sensor to facilitate maze navigation, and a color sensor to detect the end point.</p>' +
+      '<p>An electromagnet at the bottom of the robot lets you pick up magnetic objects, and a gyro helps the robot move straight.</p>',
+    longerDescription:
+      '<h3>Dimensions</h3>' +
+      '<ul>' +
+        '<li>Wheel Diameter: 5.6 cm</li>' +
+        '<li>Wheel Spacing: 11.2 cm</li>' +
+      '</ul>' +
+      '<h3>Actuators</h3>' +
+      '<ul>' +
+        '<li>Left Wheel: Port A</li>' +
+        '<li>Right Wheel: Port B</li>' +
+        '<li>Electromagnet: Port C</li>' +
+      '</ul>' +
+      '<h3>Sensors</h3>' +
+      '<ul>' +
+        '<li>Color Sensor: Port 1</li>' +
+        '<li>Ultrasonic Distance (Front): Port 2</li>' +
+        '<li>Ultrasonic Distance (Left): Port 3</li>' +
+        '<li>Ultrasonic Distance (Right): Port 4</li>' +
+        '<li>Gyro: Port 5</li>' +
+      '</ul>',
+    thumbnail: 'images/robots/maze.jpg',
+
+    bodyHeight: 4,
+    bodyWidth: 10,
+    bodyLength: 16,
+
+    wheelDiameter: 5.6,
+    wheelWidth: 0.8,
+    wheelToBodyOffset: 0.2,
+
+    bodyEdgeToWheelCenterY: 1,
+    bodyEdgeToWheelCenterZ: 2,
+
+    bodyMass: 1000,
+    wheelMass: 200,
+    casterMass: 0, // Warning: No effect due to parenting
+
+    wheelFriction: 10,
+    bodyFriction: 0,
+    casterFriction: 0, // Warning: No effect due to parenting
+
+    components: [
+      {
+        type: 'ColorSensor',
+        position: [0, -1, 9],
+        rotation: [Math.PI/2, 0, 0],
+        options: null
+      },
+      {
+        type: 'UltrasonicSensor',
+        position: [0, 2.5, 8],
+        rotation: [0, 0, 0],
+        options: null
+      },
+      {
+        type: 'UltrasonicSensor',
+        position: [-4, 2.5, 4],
+        rotation: [0, -Math.PI/2, 0],
+        options: null
+      },
+      {
+        type: 'UltrasonicSensor',
+        position: [4, 2.5, 4],
+        rotation: [0, Math.PI/2, 0],
+        options: null
+      },
+      {
+        type: 'Box',
+        position: [-7, -1, 3],
+        rotation: [0, 0, 0],
+        options: {
+          height: 3,
+          width: 1,
+          depth: 14
+        }
+      },
+      {
+        type: 'Box',
+        position: [7, -1, 3],
+        rotation: [0, 0, 0],
+        options: {
+          height: 3,
+          width: 1,
+          depth: 14
+        }
+      },
+      {
+        type: 'GyroSensor',
+        position: [0, 2.5, 5],
+        options: null
+      },
+      {
+        type: 'magnetActuator',
+        position: [0, -1, 3],
+        rotation: [0, 0, 0],
+        options: null
+      }
+    ]
+  }
 ];
