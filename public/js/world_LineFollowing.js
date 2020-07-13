411ed220d5d0ce2b3aac173d4fa37964775febff
Cort <cort@cortscorner.net>
2020-07-13 19:08:48 +0800

    * Added obstacles world

--- public/js/world_LineFollowing.js
+++ public/js/world_LineFollowing.js
@@ -15,27 +15,37 @@ var world_LineFollowing = new function() {
   this.optionsConfigurations = [
     {
       option: 'image',
-      title: 'Select Image',
+      title: 'Select Challenge',
       type: 'selectWithHTML',
       options: [
         ['Simple Curves', 'simple'],
         ['Sharp Turns', 'sharp'],
         ['Gaps 1', 'gaps1'],
-        ['Gaps 2', 'gaps2']
+        ['Gaps 2', 'gaps2'],
+        ['Obstacles 1', 'obstacles1'],
+        ['Obstacles 2', 'obstacles2']
       ],
       optionsHTML: {
         simple:
-          '<p>A simple line following map.</p>' +
+          '<p class="bold">A simple line following map.</p>' +
           '<p>You should be able to complete this using either a single sensor or double sensor line follower robot.<p>',
         sharp:
-          '<p>Line following map with sharper turns.</p>' +
+          '<p class="bold">Line following map with sharper turns.</p>' +
           '<p>It can be hard to make all the sharp turns while maintaining a smooth movement on the straights.<p>',
         gaps1:
-          '<p>Line following map with gaps.</p>' +
+          '<p class="bold">Line following map with gaps.</p>' +
           '<p>You\'ll need a double sensor line follower robot to cross these gaps.<p>',
         gaps2:
-          '<p>Line following map with gaps (hard).</p>' +
-          '<p>These gaps are wider and have only a short straight section between them.<p>'
+          '<p class="bold">Line following map with gaps (hard).</p>' +
+          '<p>These gaps are wider and have only a short straight section between them.<p>',
+        obstacles1:
+          '<p class="bold">Line following map with obstacles.</p>' +
+          '<p>The obstacles are immovable and are 20cm x 20cm.' +
+          'You\'ll need to leave the line to navigate around them.<p>',
+        obstacles2:
+          '<p class="bold">Obstacles and blocked path.</p>' +
+          '<p>Sometimes, there\'s only one way to navigate around the obstacle.</p>' +
+          '<p class="bold">This world randomizes on reset!<p>'
       }
     }
   ];
@@ -44,14 +54,18 @@ var world_LineFollowing = new function() {
     simple: 'textures/maps/Line Following/Simple.png',
     sharp: 'textures/maps/Line Following/Sharp.png',
     gaps1: 'textures/maps/Line Following/Gaps 1.png',
-    gaps2: 'textures/maps/Line Following/Gaps 2.png'
+    gaps2: 'textures/maps/Line Following/Gaps 2.png',
+    obstacles1: 'textures/maps/Line Following/Obstacles 1.png',
+    obstacles2: 'textures/maps/Line Following/Obstacles 2.png'
   };
 
   this.robotStarts = {
     simple: new BABYLON.Vector3(0, 0, -85),
     sharp: new BABYLON.Vector3(15, 0, -85),
     gaps1: new BABYLON.Vector3(0, 0, -85),
-    gaps2: new BABYLON.Vector3(15, 0, -85)
+    gaps2: new BABYLON.Vector3(15, 0, -85),
+    obstacles1: new BABYLON.Vector3(0, 0, -85),
+    obstacles2: new BABYLON.Vector3(0, 0, -135)
   }
 
   this.defaultOptions = {
@@ -99,49 +113,109 @@ var world_LineFollowing = new function() {
     self.setOptions();
   };
 
+  // Load image into map
+  this.loadImageMap = function (scene) {
+    var groundMat = new BABYLON.StandardMaterial('ground', scene);
+    var groundTexture = new BABYLON.Texture(self.imagesURL[self.options.image], scene);
+    groundMat.diffuseTexture = groundTexture;
+    groundMat.specularColor = new BABYLON.Color3(0.1, 0.1, 0.1);
+
+    var faceUV = new Array(6);
+    for (var i = 0; i < 6; i++) {
+        faceUV[i] = new BABYLON.Vector4(0, 0, 0, 0);
+    }
+    faceUV[4] = new BABYLON.Vector4(0, 0, 1, 1);
+
+    var boxOptions = {
+        width: self.options.width,
+        height: 10,
+        depth: self.options.length,
+        faceUV: faceUV
+    };
+
+    var ground = BABYLON.MeshBuilder.CreateBox('box', boxOptions, scene);
+    ground.material = groundMat;
+    ground.receiveShadows = true;
+    ground.position.y = -5;
+    ground.rotation.y = Math.PI / 2;
+
+    // Physics
+    ground.physicsImpostor = new BABYLON.PhysicsImpostor(
+      ground,
+      BABYLON.PhysicsImpostor.BoxImpostor,
+      {
+        mass: 0,
+        friction: self.options.groundFriction,
+        restitution: self.options.groundRestitution
+      },
+      scene
+    );
+  };
+
   // Create the scene
   this.load = function (scene) {
-    var options = self.options;
-
     return new Promise(function(resolve, reject) {
-      var groundMat = new BABYLON.StandardMaterial('ground', scene);
-      var groundTexture = new BABYLON.Texture(self.imagesURL[self.options.image], scene);
-      groundMat.diffuseTexture = groundTexture;
-      groundMat.specularColor = new BABYLON.Color3(0.1, 0.1, 0.1);
+      if (self.options.image == 'obstacles1') {
+        self.loadImageMap(scene);
+        self.addBox(scene, [20,20,20], [0, -48]);
+        self.addBox(scene, [20,20,20], [0, 48]);
+      } else if (self.options.image == 'obstacles2') {
+        self.loadImageMap(scene);
 
-      var faceUV = new Array(6);
-      for (var i = 0; i < 6; i++) {
-          faceUV[i] = new BABYLON.Vector4(0, 0, 0, 0);
+        self.obstacleMat = new BABYLON.StandardMaterial('obstacle', scene);
+        self.obstacleMat.diffuseColor = new BABYLON.Color3(0.8, 0.1, 0.8);
+
+        self.addBox(scene, [20,20,20], [0, -98]);
+        if (Math.random() > 0.5) {
+          self.addBox(scene, [20,30,20], [30, -123]);
+        } else {
+          self.addBox(scene, [20,30,20], [-30, -123]);
+        }
+
+        self.addBox(scene, [20,20,20], [0, -2]);
+        if (Math.random() > 0.5) {
+          self.addBox(scene, [20,30,20], [30, -2]);
+        } else {
+          self.addBox(scene, [20,30,20], [-30, -2]);
+        }
+
+        self.addBox(scene, [20,20,20], [0, 90]);
+        if (Math.random() > 0.5) {
+          self.addBox(scene, [20,30,20], [30, 115]);
+        } else {
+          self.addBox(scene, [20,30,20], [-30, 115]);
+        }
+      } else {
+        self.loadImageMap(scene);
       }
-      faceUV[4] = new BABYLON.Vector4(0, 0, 1, 1);
-
-      var boxOptions = {
-          width: options.width,
-          height: 10,
-          depth: options.length,
-          faceUV: faceUV
-      };
-
-      var ground = BABYLON.MeshBuilder.CreateBox('box', boxOptions, scene);
-      ground.material = groundMat;
-      ground.receiveShadows = true;
-      ground.position.y = -5;
-      ground.rotation.y = Math.PI / 2;
-
-      // Physics
-      ground.physicsImpostor = new BABYLON.PhysicsImpostor(
-        ground,
-        BABYLON.PhysicsImpostor.BoxImpostor,
-        {
-          mass: 0,
-          friction: options.groundFriction,
-          restitution: options.groundRestitution
-        },
-        scene
-      );
-
       resolve();
     });
+  };
+
+  // Add static box
+  this.addBox = function(scene, size, pos) {
+    var boxOptions = {
+      height: size[0],
+      width: size[1],
+      depth: size[2]
+    };
+
+    var box = BABYLON.MeshBuilder.CreateBox('obstacle', boxOptions, scene);
+    box.material = self.obstacleMat;
+    box.position.y = size[0] / 2;
+    box.position.x = pos[0];
+    box.position.z = pos[1];
+
+    box.physicsImpostor = new BABYLON.PhysicsImpostor(
+      box,
+      BABYLON.PhysicsImpostor.BoxImpostor,
+      {
+        mass: 0,
+        friction: self.options.wallFriction,
+        restitution: self.options.wallRestitution
+      },
+      scene
+    );
   };
 }
 
