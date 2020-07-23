1e9da6db735bcc943e7dcc6d6f532ae8f31a0b20
Cort <cort@cortscorner.net>
2020-07-23 15:34:54 +0800

    * Added maze2 robot

--- public/js/robotTemplates.js
+++ public/js/robotTemplates.js
@@ -267,6 +267,113 @@ var robotTemplates = [
     ]
   },
   {
+    name: 'maze2',
+    shortDescription: 'Maze Runner MkII',
+    longDescription:
+      '<p>This is an advanced maze runner robot that replaces the original\'s three ultrasonic sensor with a single laser range finder mounted on a swivel platform.</p>',
+    longerDescription:
+      '<h3>Dimensions</h3>' +
+      '<ul>' +
+        '<li>Wheel Diameter: 5.6 cm</li>' +
+        '<li>Wheel Spacing: 15.2 cm</li>' +
+      '</ul>' +
+      '<h3>Actuators</h3>' +
+      '<ul>' +
+        '<li>Port A : Left Wheel</li>' +
+        '<li>Port B : Right Wheel</li>' +
+        '<li>Port C : Electromagnet</li>' +
+        '<li>Port D : Swivel Platform</li>' +
+      '</ul>' +
+      '<h3>Sensors</h3>' +
+      '<ul>' +
+        '<li>Port 1 : Color Sensor</li>' +
+        '<li>Port 2 : Gyro</li>' +
+        '<li>Port 3 : Laser Range Sensor</li>' +
+      '</ul>',
+    thumbnail: 'images/robots/maze2.jpg',
+
+    bodyHeight: 4,
+    bodyWidth: 14,
+    bodyLength: 16,
+
+    wheelDiameter: 5.6,
+    wheelWidth: 0.8,
+    wheelToBodyOffset: 0.2,
+
+    bodyEdgeToWheelCenterY: 1,
+    bodyEdgeToWheelCenterZ: 2,
+
+    bodyMass: 1000,
+    wheelMass: 200,
+    casterMass: 0, // Warning: No effect due to parenting
+
+    wheelFriction: 10,
+    bodyFriction: 0,
+    casterFriction: 0, // Warning: No effect due to parenting
+
+    components: [
+      {
+        type: 'ColorSensor',
+        position: [0, -1, 9],
+        rotation: [Math.PI/2, 0, 0],
+        options: null
+      },
+      {
+        type: 'Box',
+        position: [-8.7, -1, 3],
+        rotation: [0, 0, 0],
+        options: {
+          height: 3,
+          width: 1,
+          depth: 14
+        }
+      },
+      {
+        type: 'Box',
+        position: [8.7, -1, 3],
+        rotation: [0, 0, 0],
+        options: {
+          height: 3,
+          width: 1,
+          depth: 14
+        }
+      },
+      {
+        type: 'GyroSensor',
+        position: [0, 2, 0],
+        options: null
+      },
+      {
+        type: 'MagnetActuator',
+        position: [0, -1, 3],
+        rotation: [0, 0, 0],
+        options: null
+      },
+      {
+        type: 'SwivelActuator',
+        position: [0, 2, 5],
+        rotation: [0, 0, 0],
+        options: null,
+        components: [
+          {
+            type: 'LaserRangeSensor',
+            position: [-0.75, 1, 0],
+            rotation: [-Math.PI/2, 0, 0],
+            options: null
+          },
+          {
+            type: 'ColorSensor',
+            position: [1, 1.25, 0],
+            rotation: [0, 0, 0],
+            options: {
+              maxRange: 30
+            }
+          },
+        ]
+      }
+    ]
+  },
+  {
     name: 'tow',
     shortDescription: 'Tow Truck',
     longDescription:
