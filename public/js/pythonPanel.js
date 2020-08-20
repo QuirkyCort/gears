473882209d6a6833c6a3f9b0af616dca89ec90b6
Cort <cort@cortscorner.net>
2020-08-20 11:02:44 +0800

    * Added autocomplete for ace

--- public/js/pythonPanel.js
+++ public/js/pythonPanel.js
@@ -23,9 +23,36 @@ var pythonPanel = new function() {
 
   // Load ace editor
   this.loadPythonEditor = function() {
+    let langTools = ace.require("ace/ext/language_tools");
     self.editor = ace.edit('pythonCode');
     self.editor.setTheme('ace/theme/monokai');
     self.editor.session.setMode('ace/mode/python');
+    self.editor.setOptions({
+      enableBasicAutocompletion: true,
+      enableSnippets: false,
+      enableLiveAutocompletion: true
+    });
+
+    var staticWordCompleter = {
+      getCompletions: function(editor, session, pos, prefix, callback) {
+        var wordList = [
+          'reflected_light_intensity', 'color', 'color_name', 'rgb', 'hsv', 'red', 'green', 'blue',
+          'angle', 'rate', 'reset',
+          'distance_centimeters',
+          'position', 'x', 'y', 'altitude',
+          'on_for_degrees', 'on_for_rotations', 'on_for_seconds', 'on', 'off'
+        ];
+        var list = wordList.map(function(word) {
+          return {
+            caption: word,
+            value: word,
+            meta: 'method'
+          };
+        })
+        callback(null, list);
+      }
+    };
+    langTools.addCompleter(staticWordCompleter);
 
     self.loadLocalStorage();
 
