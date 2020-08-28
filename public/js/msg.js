af98dbb54a8063de326184f4a4ee4751ed5b6e4c
Cort <cort@cortscorner.net>
2020-08-28 11:29:52 +0800

    * Added french translation

--- public/js/msg.js
+++ public/js/msg.js
@@ -8,80 +8,99 @@ const MSGS = {
   '#blk-motor': {
     en: 'Motor',
     tlh: 'nguSDI\'',
+    fr: 'Moteur',
   },
   '#blk-sensors': {
     en: 'Sensors',
     tlh: 'ghe\'\'or',
+    fr: 'Capteurs',
   },
   '#blk-sound': {
     en: 'Sound',
     tlh: 'wab',
+    fr: 'Sons',
   },
   '#blk-control': {
     en: 'Control',
     tlh: 'SeH',
+    fr: 'Contrôle',
   },
   '#blk-logic': {
     en: 'Logic',
     tlh: 'wanl\'',
+    fr: 'Logique',
   },
   '#blk-loops': {
     en: 'Loops',
     tlh: 'QoQ',
+    fr: 'Boucles',
   },
   '#blk-math': {
     en: 'Math',
     tlh: 'ma\'rIch',
+    fr: 'Maths',
   },
   '#blk-text': {
     en: 'Text',
     tlh: 'wej',
+    fr: 'Texte',
   },
   '#blk-lists': {
     en: 'Lists',
     tlh: 'tlhegh',
+    fr: 'Listes',
   },
   '#blk-variables': {
     en: 'Variables',
     tlh: 'qeylIS',
+    fr: 'Variables',
   },
   '#blk-functions': {
     en: 'Functions',
     tlh: 'tlhaw\'DIyuS',
+    fr: 'Fonctions',
   },
   '#blk-when_started': {
     en: 'When Started',
+    fr: 'Au démarrage',
   },
   '#main-blocks': {
     en: 'Blocks',
     tlh: 'Porgh',
+    fr: 'Blocs',
   },
   '#main-sim': {
     en: 'Simulator',
-    tlh: 'ghertlhuD'
+    tlh: 'ghertlhuD',
+    fr: 'Simulateur',
   },
   '#main-file': {
     en: 'File',
-    tlh: 'teywI\''
+    tlh: 'teywI\'',
+    fr: 'Fichier',
   },
   '#main-robot': {
     en: 'Robot',
     tlh: 'qoq',
+    fr: 'Robot',
   },
   '#main-arena': {
     en: 'Arena',
-    tlh: '\'anSa\''
+    tlh: '\'anSa\'',
+    fr: 'Arène',
   },
   '#main-help': {
     en: 'Help',
     tlh: 'QaH',
+    fr: 'Aide',
   },
   '#main-arenaTitle': {
     en: 'GearsBot Arena',
-    tlh: 'GearsBot \'anSa\''
+    tlh: 'GearsBot \'anSa\'',
+    fr: 'Arène GearsBot',
   },
   '#main-arenaDescription': {
-    en: 
+    en:
       '<p>The GearsBot Arena allows up to 4 robots to compete or cooperate with each other.</p>' +
       '<p>Program your robot using the normal GearsBot page (...where you are now), and export your program and robot as a zip package (Files -> Export Zip...).' +
       'You can then load the zip package into the GearsBot Arena and run it against other players.</p>',
@@ -89,10 +108,15 @@ const MSGS = {
       '<p>veSDuj Data\' \'e\' vIQoy.</p>' +
       '<p>tugh tlhIHtaHghach chutmey pabnIS. chol *ma\'rIgharet*, qaSpa\' je tlhIH.' +
       'tIqDu\'lIjDaq yIlan.</p>',
+    fr:
+      '<p>L\'arène GearsBot permet de faire collaborer ou de s\'affronter jusqu\'à 4 robots simultanément.</p>' +
+      '<p>Programmez votre robot depuis la page principale (...celle-ci donc), et expoertez votre programme et votre robot comme un paquet zip (Fichier -> Export Zip...).' +
+      'Vous pouvez charger le paquet zip dans l\'arène GearsBot et le lancer contre les autres joueurs.</p>',
   },
   '#main-arenaGo': {
     en: 'Go to Arena',
-    tlh: 'yIghoS'
+    tlh: 'yIghoS',
+    fr: 'En route pour l\'arène',
   }
 };
 const MSGS_KEYS = Object.keys(MSGS);
