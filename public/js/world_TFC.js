ed4a8a30090f105ab0450e35afa9635b8df8c326
Yuval Rosen <yuv.rosen@gmail.com>
2021-03-03 09:56:02 +0200

    add candle poses

--- public/js/world_TFC.js
+++ public/js/world_TFC.js
@@ -413,7 +413,10 @@ var world_TFC = new function() {
           candle = [60.5, 167, 0]; // upper right room, lower left corner
           break;
         case 9:
-          candle = [60.5, 230, 0]; // upper right room, lower left corner
+          candle = [60.5, 232, 0]; // upper right room, lower left corner
+          break;
+        case 10:
+          candle = [15, 232, 0]; // upper right room, lower left corner
           break;
         default:
           candle = [15, 15, 0]; // lower left room, lower left corner
