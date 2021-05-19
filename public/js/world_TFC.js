cfd6f1e8dd67532c63733d85e6445b52341407e4
Yuvix25 <yuv.rosen@gmail.com>
2021-05-19 10:00:03 +0300

    update random in tfc

--- public/js/world_TFC.js
+++ public/js/world_TFC.js
@@ -184,7 +184,7 @@ var world_TFC = new function() {
       if (self.options.startPos == 'top') {
         self.robotStart.position = new BABYLON.Vector3(-23, 0, 97);
       } else if (self.options.startPos == 'bottomLeft') {
-        self.robotStart.position = new BABYLON.Vector3(-5, 0, 10);
+        self.robotStart.position = new BABYLON.Vector3(-50, 0, -52.5);
       } else if (self.options.startPos == 'bottomRight') {
         let z = -(self.options.width / 2 - 12.5) + 1;
         self.robotStart.position = new BABYLON.Vector3(0, 0, z);
