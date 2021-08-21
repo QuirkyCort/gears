69c5b8c266f6af157f0d7bd06b533b1010b398f7
Cort <cort@cortscorner.net>
2021-08-21 14:20:40 +0800

    * Update cache break

--- public/js/skulpt.js
+++ public/js/skulpt.js
@@ -8,7 +8,7 @@ var skulpt = new function() {
     './ev3dev2/sensor/__init__.py': 'ev3dev2/sensor/__init__.py?v=6d1f054c',
     './ev3dev2/sensor/lego.py': 'ev3dev2/sensor/lego.py?v=64cc5ea0',
     './ev3dev2/sensor/virtual.py': 'ev3dev2/sensor/virtual.py?v=bba8d7d8',
-    './simPython.js': 'js/simPython.js?v=fce283d8'
+    './simPython.js': 'js/simPython.js?v=a4d0be46'
   };
   this.preloadedLibs = {};
 
