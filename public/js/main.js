125c14180825df3f783af3f73bf57efc50bbb287
Cort <cort@cortscorner.net>
2022-02-05 13:09:15 +0800

    * Update what's new

--- public/js/main.js
+++ public/js/main.js
@@ -920,15 +920,15 @@ var main = new function() {
 
   // Display what's new if not seen before
   this.showWhatsNew = function(forceShow=false) {
-    let current = 20220204;
+    let current = 20220205;
     let lastShown = localStorage.getItem('whatsNew');
     if (lastShown == null || parseInt(lastShown) < current || forceShow) {
       let options = {
         title: 'What\'s New',
         message:
-          '<h3>4 Feb 2022 (Hub Buttons, Movement Motors)</h3>' +
+          '<h3>5 Feb 2022 (Hub Buttons, Movement Motors)</h3>' +
           '<ul>' +
-          '<li>Hub buttons are now available for use.</li>' +
+          '<li>Hub buttons are now available for use. Find the blocks in the sensors category, and control the buttons in the simulator panel.</li>' +
           '<li>' +
           'You can now set the movement motors using blocks. ' +
           'For most people, this will be unnecessary as it will default to motors A and B which all the built-in robots uses, but it may still be useful for those with a specially configured robot.' +
