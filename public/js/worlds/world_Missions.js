3c395b00f8768bd2185e1a468bd1cc57ce37048d
Cort <cort@cortscorner.net>
2023-04-07 17:54:09 +0800

    Added WRO 2023 Junior

--- public/js/worlds/world_Missions.js
+++ public/js/worlds/world_Missions.js
@@ -21,6 +21,7 @@ var world_Missions = new function() {
       title: 'Select Mission',
       type: 'select',
       options: [
+        ['2023 WRO (Junior)', 'worlds/missions/WRO/WRO-2023-Junior.json?v=888f71dc'],
         ['2023 WRO (Elementary)', 'worlds/missions/WRO/WRO-2023-Elementary.json?v=bbda7305'],
         ['2022 FLL (Superpowered)', 'worlds/missions/FLL/FLL2022.json?v=8d10813a'],
         ['2022 WRO (Elementary)', 'worlds/missions/WRO/WRO-2022-Regular-Elementary-1.json?v=a2936cb0'],
